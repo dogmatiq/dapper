487b0a8becee7f01d200dc47ccba0ad3803bb677
James Harris <mailjamesharris@gmail.com>
2019-01-16 08:30:08 +1000

    Add support for pointers.

--- ptr.go
+++ ptr.go
@@ -10,5 +10,28 @@ func (c *context) visitPtr(
 	rv reflect.Value,
 	knownType bool,
 ) {
-	panic("not implemented")
+	rt := rv.Type()
+	marker := ""
+
+	if rv.IsNil() {
+		marker = "nil"
+	} else if c.markVisited(rv) {
+		marker = c.recursionMarker
+	}
+
+	if marker != "" {
+		if knownType {
+			c.write(w, marker)
+		} else {
+			c.write(w, "&")
+			c.write(w, rt.Elem().String())
+			c.write(w, "(")
+			c.write(w, marker)
+			c.write(w, ")")
+		}
+		return
+	}
+
+	c.write(w, "&")
+	c.visit(w, rv.Elem(), knownType)
 }
---
+++ ptr_test.go
@@ -0,0 +1,42 @@
+package dapper_test
+
+import "testing"
+
+func TestPrinter_Ptr(t *testing.T) {
+	value := 100
+	test(t, "nil pointer", (*int)(nil), "&int(nil)")
+	test(t, "non-nil pointer", &value, "&int(100)")
+}
+
+type recursive struct {
+	Name  string
+	Child *recursive
+}
+
+// This test verifies that recursive structures are detected, and do not produce
+// an infinite loop or stack overflow.
+func TestPrinter_PtrRecursion(t *testing.T) {
+	r := recursive{
+		Name: "one",
+		Child: &recursive{
+			Name: "two",
+		},
+	}
+	r.Child.Child = &r
+
+	test(
+		t,
+		"recursive structure",
+		r,
+		"dapper_test.recursive{",
+		`	Name:  "one"`,
+		"	Child: &{",
+		`		Name:  "two"`,
+		"		Child: &{",
+		`			Name:  "one"`,
+		"			Child: *recursion*",
+		"		}",
+		"	}",
+		"}",
+	)
+}
