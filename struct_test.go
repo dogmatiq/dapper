eed9ae3901c45ae79189bb5bbc838c69c2e799e9
James Harris <james.harris@icecave.com.au>
2019-01-15 20:47:08 +1000

    Add some tests for empty and anonymous structs.

--- struct_test.go
+++ struct_test.go
@@ -5,13 +5,38 @@ import "testing"
 func TestPrinter_Structs(t *testing.T) {
 	type empty struct{}
 	test(t, "empty struct", empty{}, "dapper_test.empty{}")
-	test(t, "anonymous empty struct", struct{}{}, "struct{}")
+}
+
+func TestPrinter_AnonymousStructs(t *testing.T) {
+	test(t, "empty anonymous struct", struct{}{}, "struct{}")
+
 	test(
 		t,
-		"anonymous struct",
+		"field types are shown",
 		struct{ Value int }{100},
 		`struct{
 	Value: int(100)
+}`,
+	)
+
+	type nested struct {
+		Value struct {
+			Value int
+		}
+	}
+
+	test(
+		t,
+		"field types are not shown when anonymous struct is a field in a non-anonymous struct",
+		nested{
+			Value: struct{ Value int }{
+				100,
+			},
+		},
+		`dapper_test.nested{
+	Value: {
+		Value: 100
+	}
 }`,
 	)
 }
