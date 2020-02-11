96516bd282e532de08e306f210a2528e71edcf05
Danil Petrov <danilpetrovvi@gmail.com>
2020-02-11 16:34:46 +1000

    Modify syncmap tests

--- syncmap_test.go
+++ syncmap_test.go
@@ -1,40 +1,51 @@
 package dapper_test
 
 import (
+	"errors"
+	"fmt"
+	"io"
+	"reflect"
+	"strings"
 	"sync"
 	"testing"
+
+	. "github.com/dogmatiq/dapper"
 )
 
 type syncmaps struct {
 	Map sync.Map
 }
 
-// This test verifies that that sync.Map key/value types are rendered when
-// they can be inferred from the context.
+// This test verifies that that sync.Map key/value types are always rendered.
 func TestPrinter_SyncMap(t *testing.T) {
 	var m sync.Map
 
-	test(t, "empty sync.SyncMap", &m, "*sync.Map{}")
+	test(t, "zero-value sync.Map", &m, "*sync.Map{}")
 
 	m.Store(1, 100)
 	m.Store(2, 200)
 
 	test(
 		t,
-		"*sync.SyncMap",
+		"sync.Map",
 		&m,
-		"*sync.SyncMap{",
+		"*sync.Map{",
 		"    int(1): int(100)",
 		"    int(2): int(200)",
 		"}",
 	)
+
+	m.Delete(1)
+	m.Delete(2)
+
+	test(t, "empty sync.Map", &m, "*sync.Map{}")
 }
 
 // This test verifies the formatting of sync.Map key/values in the named structs.
 func TestPrinter_SyncMapInNamedStruct(t *testing.T) {
 	test(
 		t,
-		"empty sync.Map in named struct",
+		"empty sync.Map",
 		&syncmaps{},
 		"*dapper_test.syncmaps{",
 		"    Map: {}",
@@ -48,10 +59,10 @@ func TestPrinter_SyncMapInNamedStruct(t *testing.T) {
 
 	test(
 		t,
-		"instances of *sync.Map in named struct",
+		"non-empty sync.Map",
 		sm,
-		"dapper_test.syncmaps{",
-		"    Map:               {",
+		"*dapper_test.syncmaps{",
+		"    Map: {",
 		"        int(1): int(100)",
 		"        int(2): int(200)",
 		"    }",
@@ -65,15 +76,15 @@ func TestPrinter_SyncMapKeySorting(t *testing.T) {
 	var m sync.Map
 
 	m.Store("foo", 1)
-	m.Store("bar", 200)
+	m.Store("bar", 2)
 
 	test(
 		t,
 		"keys are sorted by their string representation",
 		&m,
-		"*sync.SyncMap{",
-		`    "bar": 2`,
-		`    "foo": 1`,
+		"*sync.Map{",
+		`    "bar": int(2)`,
+		`    "foo": int(1)`,
 		"}",
 	)
 }
@@ -91,7 +102,7 @@ func TestPrinter_MultilineSyncMapKeyAlignment(t *testing.T) {
 		t,
 		"keys are aligned correctly",
 		&m,
-		"*sync.SyncMap{",
+		"*sync.Map{",
 		`    "short":                         "one"`,
 		`    "the longest key in the galaxy": "two"`,
 		"    dapper_test.multiline{",
@@ -106,7 +117,7 @@ func TestPrinter_MultilineSyncMapKeyAlignment(t *testing.T) {
 		t,
 		"keys are aligned correctly when the longest line is part of a multiline key",
 		&m,
-		"*sync.SyncMap{",
+		"*sync.Map{",
 		`    "short":                 "one"`,
 		"    dapper_test.multiline{",
 		`        Key: "multiline key"`,
@@ -125,8 +136,54 @@ func TestPrinter_SyncMapRecursion(t *testing.T) {
 		t,
 		"recursive sync.Map",
 		&m,
-		"*sync.SyncMap{",
-		`    "child": *sync.SyncMap{}(<recursion>)`,
+		"*sync.Map{",
+		`    "child": *sync.Map(<recursion>)`,
 		"}",
+	)
+}
+
+// This test verifies that recursive sync.Map is detected, and do not produce
+// an infinite loop or stack overflow.
+func TestPrinter_SyncMapFormatFunctionErr(t *testing.T) {
+	m := &sync.Map{}
+	m.Store("foo", 1)
+
+	v := Value{
+		Value:                  reflect.ValueOf(m).Elem(),
+		DynamicType:            reflect.TypeOf(m).Elem(),
+		StaticType:             reflect.TypeOf(m).Elem(),
+		IsAmbiguousDynamicType: false,
+		IsAmbiguousStaticType:  false,
+		IsUnexported:           false,
+	}
+
+	terr := errors.New("test key format function error")
+	err := SyncFilter(
+		&strings.Builder{},
+		v,
+		func(_ io.Writer, v Value) error {
+			if s, ok := v.Value.Interface().(string); ok && s == "foo" {
+				return terr
+			}
+			return nil
+		},
+	)
+
+	t.Log(fmt.Sprintf("expected:\n\n%v\n", terr))
+
+	if terr != err {
+		t.Fatal(fmt.Sprintf("actual:\n\n%v\n", err))
+	}
+
+	terr = errors.New("test value format function error")
+	err = SyncFilter(
+		&strings.Builder{},
+		v,
+		func(_ io.Writer, v Value) error {
+			if i, ok := v.Value.Interface().(int); ok && i == 1 {
+				return terr
+			}
+			return nil
+		},
 	)
 }
