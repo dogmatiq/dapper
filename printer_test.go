b255dd52befa96255291c9fa1b23a6437eede1ab
Danil Petrov <danilpetrovvi@gmail.com>
2020-02-13 15:40:15 +1000

    Add a test for Printer.Config.OmitPackagePaths.

--- printer_test.go
+++ printer_test.go
@@ -53,6 +53,52 @@ func ExamplePrinter() {
 	// }
 }
 
+func ExamplePrinter_Config() {
+	type TreeNode struct {
+		Name     string
+		Value    interface{}
+		Children []*TreeNode
+	}
+
+	type NodeValue struct{}
+
+	v := TreeNode{
+		Name: "root",
+		Children: []*TreeNode{
+			{
+				Name:  "branch #1",
+				Value: 100,
+			},
+			{
+				Name:  "branch #2",
+				Value: NodeValue{},
+			},
+		},
+	}
+
+	p := Printer{Config: Config{OmitPackagePaths: true}}
+	s := p.Format(v)
+	fmt.Println(s)
+
+	// output: dapper_test.TreeNode{
+	//     Name:     "root"
+	//     Value:    nil
+	//     Children: {
+	//         {
+	//             Name:     "branch #1"
+	//             Value:    int(100)
+	//             Children: nil
+	//         }
+	//         {
+	//             Name:     "branch #2"
+	//             Value:    dapper_test.NodeValue{}
+	//             Children: nil
+	//         }
+	//     }
+	// }
+
+}
+
 func ExamplePrint() {
 	Print(123, 456.0)
 
